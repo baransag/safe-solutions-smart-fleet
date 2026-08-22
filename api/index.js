68239a87880dfad272144dc58c632a3f7d3524ae
baransag <husnainfarooqhusnain912@gmail.com>
2026-08-22 14:58:24 +0500

    Include server directory in Vercel lambda

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-const app = require('../server/server.js');
+let app;
+try {
+  app = require('../server/server.js');
+} catch (e) {
+  console.error('CRITICAL SERVER REQUIRE ERROR:', e);
+}
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (!app) {
+    return res.status(500).json({ error: 'Server initialization failed in Vercel lambda environment.' });
+  }
+  return app(req, res);
+};
